4321006efef6cb7c03fcf419be2cc42c35f05b74
souhail-ouabou <77465337+souhail-ouabou@users.noreply.github.com>
2021-06-07 17:29:11 +0100

    ChangeEmailFormat

--- backend/controllers/sendMail.js
+++ backend/controllers/sendMail.js
@@ -20,7 +20,7 @@ const oauth2Client = new OAuth2(
 )
 
 // send mail
-const sendEmail = (to, url, txt) => {
+const sendEmail = (to, url, name, txt) => {
     oauth2Client.setCredentials({
         // access_token : MAILING_SERVICE_CLIENT_SECRET,
         refresh_token: MAILING_SERVICE_REFRESH_TOKEN,
@@ -46,13 +46,15 @@ const sendEmail = (to, url, txt) => {
         subject: "EduSpace Activation mail",
         html: `
             <div style="max-width: 700px; margin:auto; border: 10px solid #ddd; padding: 50px 20px; font-size: 110%;">
-            <h2 style="text-align: center; text-transform: uppercase;color: teal;">Welcome to EduSpace.</h2>
-            <p>Congratulations! You're almost set to start using EduSpace.
-                Just click the button below to validate your email address.
-            </p>
+            <img src="https://i.imgur.com/4jq68uE.png" alt style="display: block; margin:auto;" width="205"; >
+            <h2 style="text-align: center; text-transform: uppercase;color: rgb(37, 132, 214);">Welcome to EduSpace.</h2>
+            Hey <strong> ${name},</strong>
+            Thanks for signing up at EduSpace.   
+            To complete your registration, please confirm your email <strong>${to}</strong> by clicking the following button:
             
-            <a href=${url} style="background: crimson; text-decoration: none; color: white; padding: 10px 20px; margin: 10px 0; display: inline-block;">${txt}</a>
-        
+            <div style="text-align:center">   
+            <a href=${url} style="background: rgb(37, 132, 214); text-decoration: none; color: white; padding: 10px 20px; margin: 10px 0; display: inline-block;">${txt}</a>
+            </div>
             <p>If the button doesn't work for any reason, you can also click on the link below:</p>
         
             <div>${url}</div>
