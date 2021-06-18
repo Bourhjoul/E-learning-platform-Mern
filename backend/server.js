26b6d37cf33133f122cc7230b2ec48bb46a8054a
souhail-ouabou <77465337+souhail-ouabou@users.noreply.github.com>
2021-06-18 21:03:48 +0100

    make it better2

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-import path from 'path';
+const path = require('path')
 const dotenv = require("dotenv");
 const express = require('express');
 const mongoose = require('mongoose')
@@ -28,8 +28,8 @@ app.use((err,req,res,next)=>{
       }
    });
 })
-app.use(favicon(__dirname + '/public/favicon.ico'));
-const __dirname = path.resolve()
+
+
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static(path.join(__dirname,'/frontend/build')))
   app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname,'frontend','build','index.html')))
