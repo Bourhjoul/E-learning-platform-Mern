094c6e60b8fe00fde4a45a85f33bd9ca96c27a20
abdessamad bourhjoul <66276688+Bourhjoul@users.noreply.github.com>
2021-06-19 02:21:44 +0100

    prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -1,58 +1,66 @@
-const path = require('path')
+const path = require("path");
 const dotenv = require("dotenv");
-const express = require('express');
-const mongoose = require('mongoose')
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-const fileUpoad = require('express-fileupload')
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const fileUpoad = require("express-fileupload");
 const res = dotenv.config();
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 app.use(cors());
-app.use(cookieParser())
-app.use(fileUpoad({ useTempFiles: true }))
-
-
+app.use(cookieParser());
+app.use(fileUpoad({ useTempFiles: true }));
 
 //Routes
-app.use('/user', require('./routes/userRouter'))
-app.use('/api', require('./routes/upload'))
-app.use('/courses',require('./routes/courseRouter'))
-app.use('/orders', require('./routes/orderRoutes'))
-app.get('/api/config/paypal',(req,res)=> res.send(process.env.PAYPAL_CLIENT_ID))
-app.use((err,req,res,next)=>{
-  // because err.status is undefined 
-   res.status(404).json({
-       error : {
-           message : err.message
-      }
-   });
-})
+app.use("/user", require("./routes/userRouter"));
+app.use("/api", require("./routes/upload"));
+app.use("/courses", require("./routes/courseRouter"));
+app.use("/orders", require("./routes/orderRoutes"));
+app.get("/api/config/paypal", (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+);
+app.use((err, req, res, next) => {
+  // because err.status is undefined
+  res.status(404).json({
+    error: {
+      message: err.message,
+    },
+  });
+});
 
+const __dirname = path.resolve();
 
-if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname,'/frontend/build')))
-  app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname,'frontend','build','index.html')))
-}else{
-  app.get('/', (req,res) =>{
-      res.send('API is Runn....')
-  })
-  
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is Runn....");
+  });
 }
-
 
 const PORT = process.env.PORT || 5000;
 
+const CONNECTION_URL =
+  "mongodb+srv://souhail:souhail2001@cluster0.bnzut.mongodb.net/myDataBase?retryWrites=true&w=majority";
 
-
-const CONNECTION_URL = 'mongodb+srv://souhail:souhail2001@cluster0.bnzut.mongodb.net/myDataBase?retryWrites=true&w=majority';
-
-mongoose.connect(CONNECTION_URL, { 
-  useNewUrlParser: true, 
-  useFindAndModify: false,
-  useCreateIndex: true,
-  useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server MongoDb Connected Running on Port: http://localhost:${PORT}`)))
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(
+        `Server MongoDb Connected Running on Port: http://localhost:${PORT}`
+      )
+    )
+  )
   .catch((error) => console.log(`${error} did not connect`));
 
-mongoose.set('useFindAndModify', false); 
+mongoose.set("useFindAndModify", false);
